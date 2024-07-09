682e27d3e28b2ea371a2235fe2753a42cd910074
kubota <kubota.takeaki@gmail.com>
2024-07-09 15:33:47 +0900

    認証オフ

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,15 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
- 
-export default NextAuth(authConfig).auth;
- 
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// export default NextAuth(authConfig).auth;
+export function middleware(request: NextRequest) {
+  // 認証チェックを行わず、すべてのリクエストを通過させます
+  return NextResponse.next();
+}
+
+// オプション：ミドルウェアを特定のパスにのみ適用する場合
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
